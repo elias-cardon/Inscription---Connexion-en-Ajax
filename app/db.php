384209869932891e6c144b3f149e53d4b00dcb89
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-12-07 15:38:34 +0100

    Rangement du code

--- app/db.php
+++ app/db.php
@@ -3,14 +3,13 @@
  * Database settings
  */
 define("DB_SERVER", "localhost");
-define("DB_NAME", "todolist");
+define("DB_NAME", "");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
 
 try {
-    $db = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USERNAME, DB_PASSWORD);
-}
-catch (PDOException $e) {
+    $db = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+} catch (PDOException $e) {
     echo "Error!: " . $e->getMessage() . "<br/>";
     exit();
 }
